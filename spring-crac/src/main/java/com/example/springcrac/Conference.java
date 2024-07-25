9cab1c3e663bb989f0add45b21f2d36e9c18872b
Maximillian Arruda <dearrudam@gmail.com>
2024-07-25 10:59:19 -0300

    chore: n+1 solution with JPA EntityGraph
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- spring-crac/src/main/java/com/example/springcrac/Conference.java
+++ spring-crac/src/main/java/com/example/springcrac/Conference.java
@@ -1,9 +1,35 @@
 package com.example.springcrac;
 
+import jakarta.persistence.EntityGraph;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-@Repository
-public interface Conference extends JpaRepository<Attendee, Long> {
+import java.util.List;
+
+@Service
+public class Conference {
+
+    @Autowired
+    EntityManager em;
+
+    @Transactional(readOnly = true)
+    List<Attendee> findAll() {
+
+
+        EntityGraph<Attendee> entityGraph = em.createEntityGraph(Attendee.class);
+        entityGraph.addAttributeNodes("technologies");
+
+        TypedQuery<Attendee> query = em.createQuery("select a from Attendee a", Attendee.class);
+
+        query.setHint("jakarta.persistence.loadgraph", entityGraph);
+
+        return query.getResultList();
+    }
 
 }
